ec1b0fc9872f0c57c996cc7d9ce1bcdac4692344
imonlinux <39863321+imonlinux@users.noreply.github.com>
2024-02-21 16:39:49 -0800

    Add files via upload

--- custom_components/apsystems_power_local/__init__.py
+++ custom_components/apsystems_power_local/__init__.py
@@ -1,2 +1,14 @@
-def setup(hass, config):
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
--- custom_components/apsystems_power_local/sensor.py
+++ custom_components/apsystems_power_local/sensor.py
@@ -7,7 +7,7 @@ from homeassistant.components.sensor import (
     SensorDeviceClass,
     SensorStateClass
 )
-from homeassistant.const import UnitOfEnergy
+from homeassistant.const import UnitOfEnergy  # Ensure using updated constants
 
 # Define the minimum time interval between updates
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
@@ -27,37 +27,18 @@ class APSystemsSensor(SensorEntity):
     def __init__(self, ip_address):
         self._state = None
         self._ip_address = ip_address
-        self._name = "APSystems Power"
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def unique_id(self):
-        return f"apsystems_power_{self._ip_address}"
-
-    @property
-    def device_class(self):
-        return SensorDeviceClass.ENERGY
-
-    @property
-    def state_class(self):
-        return SensorStateClass.TOTAL_INCREASING
-
-    @property
-    def unit_of_measurement(self):
-        return UnitOfEnergy.KILO_WATT_HOUR
-
-    @property
-    def state(self):
-        return self._state
+        self._attr_name = "APSystems Power"
+        self._attr_unique_id = f"apsystems_power_{self._ip_address.replace('.', '_')}"
+        self._attr_device_class = SensorDeviceClass.ENERGY
+        self._attr_state_class = SensorStateClass.TOTAL_INCREASING
+        self._attr_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     async def async_update(self):
         url = f"http://{self._ip_address}/index.php/realtimedata/power_graph"
         self._state = await self.hass.async_add_executor_job(scrape_solar_data, url)
 
-def setup_platform(hass, config, async_add_entities, discovery_info=None):
-    ip_address = config.get('ip_address')
+async def async_setup_entry(hass, entry, async_add_entities):
+    ip_address = entry.data["ip_address"]
     async_add_entities([APSystemsSensor(ip_address)], True)
+
