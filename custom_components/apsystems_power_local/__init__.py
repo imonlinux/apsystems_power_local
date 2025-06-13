82756eedc8663d46d2492c29ab6298c42f51ad00
imonlinux <39863321+imonlinux@users.noreply.github.com>
2025-06-12 21:21:39 -0700

    Update __init__.py

--- custom_components/apsystems_power_local/__init__.py
+++ custom_components/apsystems_power_local/__init__.py
@@ -8,7 +8,5 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
