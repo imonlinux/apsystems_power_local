7b17b7ffb8a2b7dbf9cfff65ec3c0f53d072119d
imonlinux <39863321+imonlinux@users.noreply.github.com>
2025-06-14 21:17:27 -0700

    Update __init__.py

--- custom_components/apsystems_power_local/__init__.py
+++ custom_components/apsystems_power_local/__init__.py
@@ -1,5 +1,6 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+
 from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict):
@@ -11,5 +12,4 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # Properly unload platform when integration is removed
     return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
