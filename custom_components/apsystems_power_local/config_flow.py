3bf4f7873b7fa3e121d14020faefdddb3585123e
imonlinux <39863321+imonlinux@users.noreply.github.com>
2025-11-28 13:31:39 -0800

    Update config_flow.py
    Add function to suspend after sunset and start after sunrise.

--- custom_components/apsystems_power_local/config_flow.py
+++ custom_components/apsystems_power_local/config_flow.py
@@ -1,26 +1,32 @@
 from homeassistant import config_entries
+from homeassistant.core import callback
 import voluptuous as vol
-from .const import DOMAIN, DEFAULT_NAME
 import ipaddress
+
+from .const import DOMAIN, DEFAULT_NAME, CONF_IP_ADDRESS
+from .options_flow import APSystemsOptionsFlow
+
 
 class APSystemsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
         errors = {}
+
         if user_input is not None:
             try:
-                ipaddress.ip_address(user_input["ip_address"])
+                ipaddress.ip_address(user_input[CONF_IP_ADDRESS])
+
+                # If we get here, IP is valid – create the entry
                 return self.async_create_entry(
                     title=DEFAULT_NAME,
                     data=user_input,
                 )
             except ValueError:
-                errors["ip_address"] = "invalid_ip"
+                errors[CONF_IP_ADDRESS] = "invalid_ip"
 
         fields = {
-            vol.Required("ip_address", default=""): str,
+            vol.Required(CONF_IP_ADDRESS, default=""): str,
         }
 
         return self.async_show_form(
@@ -28,3 +34,9 @@ class APSystemsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(fields),
             errors=errors,
         )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: config_entries.ConfigEntry):
+        """Return the options flow handler."""
+        return APSystemsOptionsFlow(config_entry)
