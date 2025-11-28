fe29b5b049b1a0806afd69cb8715ac439f81e266
imonlinux <39863321+imonlinux@users.noreply.github.com>
2025-11-28 12:49:07 -0800

    Update const.py
    Add function to suspend after sunset and start after sunrise.

--- custom_components/apsystems_power_local/const.py
+++ custom_components/apsystems_power_local/const.py
@@ -1,2 +1,6 @@
 DOMAIN = "apsystems_power_local"
 DEFAULT_NAME = "APSystems Power"
+
+CONF_IP_ADDRESS = "ip_address"
+CONF_SUSPEND_AFTER_SUNSET = "suspend_after_sunset"
+DEFAULT_SUSPEND_AFTER_SUNSET = False
